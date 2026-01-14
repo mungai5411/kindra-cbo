7a24a74f807520b78b10a7a18eec315722502044
Mosesmungai <yourname@example.com>
2026-01-14 12:27:33 +0300

    Fix: Remove django_extensions from INSTALLED_APPS

--- backend/kindra_cbo/settings.py
+++ backend/kindra_cbo/settings.py
@@ -78,7 +78,6 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'django_celery_beat',
     'django_celery_results',
-    'django_extensions',  # For HTTPS development server
     
     # Kindra CBO Apps
     'accounts.apps.AccountsConfig',
