a9707a614bf7451cf73aad396d2df5bcd5ec6c56
Mosesmungai <yourname@example.com>
2026-01-24 09:10:51 +0300

    Remove unused useTheme import in Overview.tsx

--- frontend/src/components/dashboard/Overview.tsx
+++ frontend/src/components/dashboard/Overview.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, CircularProgress, useTheme } from '@mui/material';
+import { Box, Typography, CircularProgress } from '@mui/material';
 
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
